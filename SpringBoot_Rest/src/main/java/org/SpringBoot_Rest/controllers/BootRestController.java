2ff8d8e3d86626200a2cb59bb0c2a6d4a772b803
Abhi <abhim.moharana@gmail.com>
2019-04-24 11:56:21 +0530

    added for retriving a single object by RESTful web services and SpringBoot

--- SpringBoot_Rest/src/main/java/org/SpringBoot_Rest/controllers/BootRestController.java
+++ SpringBoot_Rest/src/main/java/org/SpringBoot_Rest/controllers/BootRestController.java
@@ -3,14 +3,21 @@ package org.SpringBoot_Rest.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.SpringBoot_Rest.beans.Country;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BootRestController
 {
-   @GetMapping(value="/countries", headers="Accept=application/json")
+   @GetMapping(value="/countries")
+   @ResponseBody
    public List<Country> getAllCountries()
    {
 	   List<Country> countries=new ArrayList<Country>();
@@ -18,7 +25,18 @@ public class BootRestController
 	   return countries;
    }
    
-   
+   @GetMapping(value="/countries/{id}",headers="Accept=application/json")
+   public Country getCountry(@PathVariable int id)
+   {
+	   List<Country> countries=new ArrayList<Country>();
+	   countries=returnAllcountries();
+	   for(Country country:countries)
+	   {
+		   if(country.getId()==id)
+			   return country;
+	   }
+	   return null;
+   }
    
    
    
