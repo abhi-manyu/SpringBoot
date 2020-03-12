537dd1ed6a7215ea7f831b397b5521557a8370f2
Abhi <abhim.moharana@gmail.com>
2020-03-12 13:24:02 +0530

    new sample project for calling http request from spring boot and accessing .csv file from web

---
+++ Tracker/src/test/java/com/example/newVersion/NewVersion/NewVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newVersion.NewVersion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
