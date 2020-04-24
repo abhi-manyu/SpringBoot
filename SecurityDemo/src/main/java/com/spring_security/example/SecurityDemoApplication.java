8707c8ba5521b15e9a4c1824cd8962e561bb2e03
Abhimanyu <abhim.moharana@gmail.com>
2020-04-24 17:44:00 +0530

    demonstrating the diffrence btween spring boot version 1 and 2 in terms of the Spring security

--- SecurityDemo/src/main/java/com/spring_security/example/SecurityDemoApplication.java
+++ SecurityDemo/src/main/java/com/spring_security/example/SecurityDemoApplication.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
+// here we are using properties file for specifying user name and pwd.
+// so for restrict Spring boot to generate its own default password used 
+// following configuration with @SpringBootApplication
+
+@SpringBootApplication//(exclude= {SecurityAutoConfiguration.class})
    // we can disable spring boot securityAutoConfiguration to b applied by the above way 
    // or else in application.properties also we can write and disable that.
+   
 public class SecurityDemoApplication
 {
 	public static void main(String[] args)
