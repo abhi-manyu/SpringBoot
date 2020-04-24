c05c9e26404a490f9c7e668d5cb4d6d4a18ef9ba
Abhimanyu <abhim.moharana@gmail.com>
2020-04-24 19:31:16 +0530

    restored back to spring security as usual

--- SecurityDemo/src/main/java/com/spring_security/example/auth/SpringSecurity.java
+++ SecurityDemo/src/main/java/com/spring_security/example/auth/SpringSecurity.java
@@ -1,36 +1,31 @@
-/*
- * package com.spring_security.example.auth;
- * 
- * import org.springframework.context.annotation.Configuration; import
- * org.springframework.http.HttpMethod; import
- * org.springframework.security.config.annotation.authentication.builders.
- * AuthenticationManagerBuilder; import
- * org.springframework.security.config.annotation.web.builders.HttpSecurity;
- * import org.springframework.security.config.annotation.web.configuration.
- * EnableWebSecurity; import
- * org.springframework.security.config.annotation.web.configuration.
- * WebSecurityConfigurerAdapter;
- * 
- * @Configuration
- * 
- * @EnableWebSecurity public class SpringSecurity extends
- * WebSecurityConfigurerAdapter {
- * 
- * @Override protected void configure(AuthenticationManagerBuilder auth)throws
- * Exception { auth.inMemoryAuthentication() .withUser("user")
- * .password("Reset123") .roles("USER") .and() .withUser("admin")
- * .password("abhi@manyu") .roles("ADMIN") .and() .withUser("manager")
- * .password("manager") .roles("MANAGER"); }
- * 
- * @Override protected void configure(HttpSecurity http)throws Exception {
- * http.httpBasic() .and() .authorizeRequests()
- * .antMatchers(HttpMethod.GET,"/products/**").hasAnyRole("ADMIN","MANAGER",
- * "USER")
- * .antMatchers(HttpMethod.GET,"/products/{id}/**").hasAnyRole("USER","MANAGER",
- * "ADMIN")
- * .antMatchers(HttpMethod.POST,"/products/**").hasAnyRole("ADMIN","MANAGER")
- * .antMatchers(HttpMethod.PUT,"/products/{id}/**").hasAnyRole("ADMIN",
- * "MANAGER")
- * .antMatchers(HttpMethod.DELETE,"/products/{id}/**").hasRole("MANAGER") .and()
- * .csrf().disable() .formLogin().disable(); } }
- */
+
+package com.spring_security.example.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+
+@EnableWebSecurity
+public class SpringSecurity extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("Reset123").roles("USER").and().withUser("admin")
+				.password("abhi@manyu").roles("ADMIN").and().withUser("manager").password("manager").roles("MANAGER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic().and().authorizeRequests().antMatchers(HttpMethod.GET, "/products/**")
+				.hasAnyRole("ADMIN", "MANAGER", "USER").antMatchers(HttpMethod.GET, "/products/{id}/**")
+				.hasAnyRole("USER", "MANAGER", "ADMIN").antMatchers(HttpMethod.POST, "/products/**")
+				.hasAnyRole("ADMIN", "MANAGER").antMatchers(HttpMethod.PUT, "/products/{id}/**")
+				.hasAnyRole("ADMIN", "MANAGER").antMatchers(HttpMethod.DELETE, "/products/{id}/**").hasRole("MANAGER")
+				.and().csrf().disable().formLogin().disable();
+	}
+}
