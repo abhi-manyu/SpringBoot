302efbbcbe611895ead710c38da66406f7a65a5e
Abhi <abhim.moharana@gmail.com>
2021-04-06 16:30:01 +0530

    branch head adjustment

--- Revise/src/main/java/com/example/demo/controllers/SignInController.java
+++ Revise/src/main/java/com/example/demo/controllers/SignInController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import javax.mail.MessagingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,10 @@ public class SignInController
 	private MailService mailServ;
 	
 	@GetMapping(value="/sendMail")
-	public void sendmail()
+	public void sendmail() throws MessagingException
 	{
-		mailServ.sendMail();
+		//mailServ.sendMail();
+		mailServ.sendHtmlMessage();
 		System.out.println("mail sent successfully");
 	}
 }
--- Revise/src/main/java/com/example/demo/service/MailService.java
+++ Revise/src/main/java/com/example/demo/service/MailService.java
@@ -1,8 +1,16 @@
 package com.example.demo.service;
 
+import java.io.File;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,14 +18,54 @@ public class MailService {
 
 	@Autowired
 	private JavaMailSender mailSender;
-	public void sendMail()
+	/*public void sendMail()
 	{
 		SimpleMailMessage  msg = new SimpleMailMessage();
 		
 		msg.setTo("abhimanyumoharana144@gmail.com");
 		msg.setSubject("testing spring boot mail");
-		msg.setText("this is a test mail from spring boot");
+		msg.setText("<h2>test mail from spring boot</h2>"
+				+ "<p>this is a test mail from spring boot</p>");
 		
 		mailSender.send(msg);
 	}
+	*/
+	
+	/*public void sendmailwithAttachment() throws MessagingException
+	{
+		MimeMessage message = mailSender.createMimeMessage();
+		
+		MimeMessageHelper mimMesageHelper = 
+				new MimeMessageHelper(message, true);
+		
+		mimMesageHelper.setSubject("testing with attachment");
+		mimMesageHelper.setTo("abhimanyumoharana144@gmail.com");
+		mimMesageHelper.setText("this is to check is attachments are working in spring boot");
+		
+		FileSystemResource file =
+				new FileSystemResource(new File("C:\\abhi\\GIT_HUB\\GIT\\GIT_local\\SpringBoot\\Revise\\src\\main\\java\\attachment.txt"));
+		mimMesageHelper.addA
+		
+	}*/
+	
+	
+	
+	public void sendHtmlMessage() throws MessagingException
+	{
+		MimeMessage msg = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(msg);
+		helper.setSubject("this is a html message");
+		helper.setTo("abhimanyumoharana144@gmail.com");
+		
+		boolean html = true;
+		
+		helper.setText("<h2>test mail from spring boot</h2>"
+				+ "<p>this is a test mail from spring boot</p>"
+				+ "<u><a href = \"www.google.com\">"
+				+ "if want to unsubscribe please click on below link</></u>",html);
+		mailSender.send(msg);
+	}
+	
+	
+	
 }
